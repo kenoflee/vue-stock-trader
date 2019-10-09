98fd25a3c7c652463b2c86bb4a36287bf01aa834
Kenneth Lee <kennethlee@Kenneths-MacBook-Pro.local>
2019-10-09 14:28:52 -0700

    set up components and bootstrap-vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class="container">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Home from './components/Home.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Home,
+  }
+}
+</script>
+
+<style lang="scss">
+@import 'node_modules/bootstrap/scss/bootstrap';
+@import 'node_modules/bootstrap-vue/src/index.scss';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">Stock Trader</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+            <b-nav-item href="#">Portfolio</b-nav-item>
+            <b-nav-item href="#">Stocks</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#">End Day</b-nav-item>
+        </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
