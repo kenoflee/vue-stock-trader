1a5dea99ab7a35daf59462eea1f4af2fc869fa81
Kenneth Lee <kennethlee@Kenneths-MacBook-Pro.local>
2019-10-09 17:09:06 -0700

    clean up

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,11 @@
 
 <script>
 import Header from './components/Header.vue';
-import Home from './components/Home.vue'
 
 export default {
   name: 'app',
   components: {
     Header,
-    Home,
   }
 }
 </script>
