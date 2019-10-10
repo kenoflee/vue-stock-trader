cdd63a223c2eccd5f3e7926b9f42d658cfc914f8
Kenneth Lee <kennethlee@Kenneths-MacBook-Pro.local>
2019-10-09 18:27:35 -0700

    message for empty portfolio page

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="Component">
+    <div class="Component" v-if="this.$store.state.portfolio.length > 0">
         <Stock
             v-for="stock in this.$store.state.portfolio"
             :key="stock.name" type="sell"
             :name="stock.name"
             :price="stock.price"/>
     </div>
+    <p v-else>You have no stocks in your portfolio.</p>
 </template>
 
 <script>
